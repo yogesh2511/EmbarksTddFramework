0e0a1d7cda2c1ce109a01e0b638ee36c3219786f
yogesh2511 <yogeshsolanki25@gmail.com>
2025-04-07 22:43:53 +0530

    Signed-off-by: yogesh2511 <yogeshsolanki25@gmail.com>

--- src/test/java/listeners/ReportHelperListener.java
+++ src/test/java/listeners/ReportHelperListener.java
@@ -23,156 +23,175 @@ import utilities.ScreenshotUtil;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
 
 public class ReportHelperListener implements ITestListener {
 
-    private ExcelReader excel;
-    private final String sheetName = "TestResults";
-    private int testCounter = 1;
-    private int executionNumber = 1;
-    private String excelPath;
-    
-    static Date d = new Date();
+	private ExcelReader excel;
+	private final String sheetName = "TestResults";
+	private int testCounter = 1;
+	private int executionNumber = 1;
+	private String excelPath;
+
+	static Date d = new Date();
 	static String fileName = "Extent_" + d.toString().replace(":", "_").replace(" ", "_") + ".html";
 	private static ExtentReports extent = ExtentManager
 			.createInstance(System.getProperty("user.dir") + "\\report\\" + fileName);
 	public static ExtentTest test;
 
-	
-    @Override
-    public void onStart(ITestContext context) {
-        try {
-            // Excel file setup
-            String reportFolderPath = PathManager.getResourcePath("/report/");
-            File reportDir = new File(reportFolderPath);
-            if (!reportDir.exists()) {
-                reportDir.mkdirs();
-            }
+	@Override
+	public void onStart(ITestContext context) {
+		try {
+			// Excel file setup
+			String reportFolderPath = PathManager.getResourcePath("/report/");
+			File reportDir = new File(reportFolderPath);
+			if (!reportDir.exists()) {
+				reportDir.mkdirs();
+			}
 
-            excelPath = reportFolderPath + "testResult.xlsx";
-            File excelFile = new File(excelPath);
+			excelPath = reportFolderPath + "testResult.xlsx";
+			File excelFile = new File(excelPath);
 
-            if (!excelFile.exists()) {
-                XSSFWorkbook workbook = new XSSFWorkbook();
-                FileOutputStream fileOut = new FileOutputStream(excelFile);
-                workbook.write(fileOut);
-                fileOut.close();
-                workbook.close();
-                System.out.println("Created new Excel file: " + excelPath);
-            }
+			if (!excelFile.exists()) {
+				XSSFWorkbook workbook = new XSSFWorkbook();
+				FileOutputStream fileOut = new FileOutputStream(excelFile);
+				workbook.write(fileOut);
+				fileOut.close();
+				workbook.close();
+				System.out.println("Created new Excel file: " + excelPath);
+			}
 
-            excel = new ExcelReader(excelPath);
+			excel = new ExcelReader(excelPath);
 
-            if (!excel.isSheetExist(sheetName)) {
-                excel.addSheet(sheetName);
-            }
+			if (!excel.isSheetExist(sheetName)) {
+				excel.addSheet(sheetName);
+			}
 
-            excel.addColumn(sheetName, "Execution No.");
-            excel.addColumn(sheetName, "TestName");
-            excel.addColumn(sheetName, "Status");
-            excel.addColumn(sheetName, "Date");
-            excel.addColumn(sheetName, "Time");
+			excel.addColumn(sheetName, "Execution No.");
+			excel.addColumn(sheetName, "TestName");
+			excel.addColumn(sheetName, "Status");
+			excel.addColumn(sheetName, "Date");
+			excel.addColumn(sheetName, "Time");
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-    @Override
-    public void onTestStart(ITestResult result) {
-    	test = extent
+	@Override
+	public void onTestStart(ITestResult result) {
+		test = extent
 				.createTest(result.getTestClass().getName() + "     @TestCase : " + result.getMethod().getMethodName());
-    	
-    }
 
-    @Override
-    public void onTestSuccess(ITestResult result) {
-        writeTestResult(result, "PASS");
-    	String methodName = result.getMethod().getMethodName();
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		writeTestResult(result, "PASS");
+		String methodName = result.getMethod().getMethodName();
+		String screenshotPath = ScreenshotUtil.captureScreenshot(methodName);
 		String logText = "<b>" + "TEST CASE:- " + methodName.toUpperCase() + " PASSED" + "</b>";
+		try {
+
+			test.pass("<b><font color=green>" + "Screenshot of PASSED" + "</font></b><br>",
+					MediaEntityBuilder.createScreenCaptureFromPath(screenshotPath).build());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 		Markup m = MarkupHelper.createLabel(logText, ExtentColor.GREEN);
 		test.pass(m);
 
-    }
+	}
 
-    @Override
-    public void onTestFailure(ITestResult result) {
-    	String methodName = result.getMethod().getMethodName();
-        String screenshotPath = ScreenshotUtil.captureScreenshot(methodName);
-        File imageFile = new File(screenshotPath);
-        String base64String = encodeFileToBase64Binary(imageFile);
-        String base64Image = "data:image/png;base64," + base64String;
+	@Override
+	public void onTestFailure(ITestResult result) {
+		String methodName = result.getMethod().getMethodName();
+		String screenshotPath = ScreenshotUtil.captureScreenshot(methodName);
+		File imageFile = new File(screenshotPath);
+		String base64String = encodeFileToBase64Binary(imageFile);
+		String base64Image = "data:image/png;base64," + base64String;
 
-        String excepionMessage = Arrays.toString(result.getThrowable().getStackTrace());
+		String excepionMessage = Arrays.toString(result.getThrowable().getStackTrace());
 		test.fail(excepionMessage);
 		String logText = "<b>" + "TEST CASE:- " + methodName.toUpperCase() + " FAILED" + "</b>";
-    	try {
-	         writeTestResult(result, "FAIL");
-	         String screenshot = base64Image;
-				test.fail("<b><font color=red>" + "Screenshot of failure" + "</font></b><br>",
-						MediaEntityBuilder.createScreenCaptureFromPath(screenshotPath).build());
-				test.addScreenCaptureFromBase64String(base64Image, methodName + " Screenshot");
-	        
-        } catch (Exception e) {
-            LoggerUtils.info("Exception during failure reporting: " + e.getMessage());
-            e.printStackTrace();
-        }
-    	Markup m = MarkupHelper.createLabel(logText, ExtentColor.RED);
+		try {
+			writeTestResult(result, "FAIL");
+			String screenshot = base64Image;
+			test.fail("<b><font color=red>" + "Screenshot of failure" + "</font></b><br>",
+					MediaEntityBuilder.createScreenCaptureFromPath(screenshotPath).build());
+			test.addScreenCaptureFromBase64String(base64Image, methodName + " Screenshot");
+
+		} catch (Exception e) {
+			LoggerUtils.info("Exception during failure reporting: " + e.getMessage());
+			e.printStackTrace();
+		}
+		Markup m = MarkupHelper.createLabel(logText, ExtentColor.RED);
 		test.log(Status.FAIL, m);
-    }
+	}
 
-    @Override
-    public void onTestSkipped(ITestResult result) {
-         writeTestResult(result, "SKIPPED");
-         String methodName = result.getMethod().getMethodName();
- 		String logText = "<b>" + "Test Case:- " + methodName + " Skipped" + "</b>";
- 		Markup m = MarkupHelper.createLabel(logText, ExtentColor.YELLOW);
- 		test.skip(m);
-    }
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		writeTestResult(result, "SKIPPED");
+		String methodName = result.getMethod().getMethodName();
+		String screenshotPath = ScreenshotUtil.captureScreenshot(methodName);
+		String logText = "<b>" + "Test Case:- " + methodName + " Skipped" + "</b>";
+		try {
 
-    @Override
-    public void onFinish(ITestContext context) {
-        if (excel != null) {
-            excel.closeWorkbook();
-        }
-        if (extent != null) {
+			test.skip("<b><font color=grey>" + "Screenshot of Skipped" + "</font></b><br>",
+					MediaEntityBuilder.createScreenCaptureFromPath(screenshotPath).build());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Markup m = MarkupHelper.createLabel(logText, ExtentColor.YELLOW);
+		test.skip(m);
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
+		if (excel != null) {
+			excel.closeWorkbook();
+		}
+		if (extent != null) {
 			extent.flush();
 		}
-    }
+	}
 
-    private void writeTestResult(ITestResult result, String status) {
-        try {
-            String testName = result.getMethod().getMethodName();
-            String date = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
-            String time = new SimpleDateFormat("HH:mm:ss").format(new Date());
+	private void writeTestResult(ITestResult result, String status) {
+		try {
+			String testName = result.getMethod().getMethodName();
+			String date = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+			String time = new SimpleDateFormat("HH:mm:ss").format(new Date());
 
-            int rowNum = testCounter + 1;
+			int rowNum = testCounter + 1;
 
-            excel.setCellData(sheetName, "Execution No.", rowNum, String.valueOf(executionNumber));
-            excel.setCellData(sheetName, "TestName", rowNum, testName);
-            excel.setCellData(sheetName, "Status", rowNum, status);
-            excel.setCellData(sheetName, "Date", rowNum, date);
-            excel.setCellData(sheetName, "Time", rowNum, time);
+			excel.setCellData(sheetName, "Execution No.", rowNum, String.valueOf(executionNumber));
+			excel.setCellData(sheetName, "TestName", rowNum, testName);
+			excel.setCellData(sheetName, "Status", rowNum, status);
+			excel.setCellData(sheetName, "Date", rowNum, date);
+			excel.setCellData(sheetName, "Time", rowNum, time);
 
-            testCounter++;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-    
-    private static String encodeFileToBase64Binary(File file) {
-        String encodedFile = null;
-        try (FileInputStream fis = new FileInputStream(file)) {
-            byte[] bytes = new byte[(int) file.length()];
-            fis.read(bytes);
-            encodedFile = new String(Base64.encodeBase64(bytes), "UTF-8");
-        } catch (Exception e) {
-            LoggerUtils.info("Base64 encoding failed: " + e.getMessage());
-            e.printStackTrace();
-        }
-        return encodedFile;
-    }
+			testCounter++;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static String encodeFileToBase64Binary(File file) {
+		String encodedFile = null;
+		try (FileInputStream fis = new FileInputStream(file)) {
+			byte[] bytes = new byte[(int) file.length()];
+			fis.read(bytes);
+			encodedFile = new String(Base64.encodeBase64(bytes), "UTF-8");
+		} catch (Exception e) {
+			LoggerUtils.info("Base64 encoding failed: " + e.getMessage());
+			e.printStackTrace();
+		}
+		return encodedFile;
+	}
 }
